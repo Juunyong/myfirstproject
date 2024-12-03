c646cd78f94d3954c235d2a3697cff1c0005127a
Juunyong <qkrwnsdyd212@gmail.com>
2024-12-03 17:52:23 +0900

    fin

--- resource/js/jquery.js
+++ resource/js/jquery.js
@@ -5,7 +5,7 @@ $(function () {
 });
 
 $(function () {
-    $('.nav-head__menutoggle is_menu__open').on('click', function () {
+    $('.nav-head__menutoggle').on('click', function () {
         $('.nav-head__menutoggle').removeClass('is_menu__open');
     });
 });
